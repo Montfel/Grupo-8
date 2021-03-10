cb38c3bb334b90cc58b862109352e145b01fb865
Cássio Figueiredo <57011185+CassioFig@users.noreply.github.com>
2021-03-09 21:51:17 -0300

    Configurações iniciais do frontend.

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+
+    </div>
+</template>
+
+<script>
+import { baseApiUrl, userKey} from '@/global.js'
+import axios from 'axios'
+
+export default {
+    name: "App",
+    data: function() {
+        return {
+
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
