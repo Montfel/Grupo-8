b3a88dd92dc765254a72430a26e7b98944f4d993
Caio Leandro <caioleandro38@gmail.com>
2021-03-11 22:29:08 -0300

    Exclusão das rotas das antigas API's

--- backend/config/rotas.js
+++ backend/config/rotas.js
@@ -2,15 +2,6 @@ module.exports = app => {
     app.post('/signin', app.api.autenticacao.signin)
     app.post('/validateToken', app.api.autenticacao.validateToken)
 
-    app.route('/medico')
-        .post(app.api.medico.salvar)
-
-    app.route('/paciente')
-        .post(app.api.paciente.salvar)
-
-    app.route('/pessoa')
-        .post(app.api.pessoa.salvar)
-
     app.route('/usuario')
         .post(app.api.usuario.salvar) 
 }
