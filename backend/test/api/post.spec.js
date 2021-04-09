7f247f5b8335c6a8eac6c917a0395831fbc42578
Paulodev88 <paulo.ricardodev88@gmail.com>
2021-04-08 21:22:17 -0300

    método post/usario em teste

--- backend/test/api/post.spec.js
+++ backend/test/api/post.spec.js
@@ -1,30 +1,30 @@
-// let chai = require("chai");
-// let chaiHttp = require("chai-http");
-// const app =  require("../../index");
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+const app =  require("../../index");
 
-// //Assertion Style
-// chai.should();
+//Assertion Style
+chai.should();
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// describe("POST /registro/usuario", () => {
-//     it("É possivél criar um novo Usuario", (done) => {
+describe("POST /registro/usuario", () => {
+    it("É possivél criar um novo Usuario", (done) => {
 
-//         const Usuario = {
+        const Usuario = {
                       
-//                 cpf: "123456782901",
-//                 nome: "Paulo11",
-//                 senha: "12345611",
-//                 confirmacaoSenha: "12345611"
-//             }
+                cpf: "123456782901",
+                nome: "Paulo11",
+                senha: "12345611",
+                confirmacaoSenha: "12345611"
+            }
 
-//         chai.request(app)                
-//             .post("/registro/usuario")
-//             .send(Usuario)
-//             .end((err, response) => {
-//                 response.should.have.status(204);
-//             done();
-//             });
-//     });
+        chai.request(app)                
+            .post("/registro/usuario")
+            .send(Usuario)
+            .end((err, response) => {
+                response.should.have.status(204);
+            done();
+            });
+    });
 
-// })
+})
