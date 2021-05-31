76d766f492cd5e08e0ca9657a6ff7ad490a65bbd
Cássio Figueiredo <57011185+CassioFig@users.noreply.github.com>
2021-05-31 18:59:29 -0300

    Update EnvioLaudo.vue

--- frontend/src/components/registros/EnvioLaudo.vue
+++ frontend/src/components/registros/EnvioLaudo.vue
@@ -5,34 +5,83 @@
             <h2>Registro do Laudo</h2>
         </div>
         <label for="input-cpf">CRM:</label>
-                <b-input id="input-crm" placeholder="Informe seu CRM"
-                type="number" min="0"></b-input>
+        <b-input id="input-crm" placeholder="Informe seu CRM"
+        type="number" min="0" v-model="laudo.crm"></b-input>
+
         <br>
+
         <label for="input-cpf">CPF:</label>
-                <b-input id="input-cpf" placeholder="Informe o CPF do paciente"
-                type="number" min="0"></b-input>
+        <b-input id="input-cpf" placeholder="Informe o CPF do paciente"
+        type="number" min="0" v-model="laudo.cpf"></b-input>
+
         <br>
+
         <label for="input-data-exame">Data do Exame:</label>
-                <b-input id="input-data-exame" placeholder="Informe a data do exame" type="date"></b-input>
-                <br>
+        <b-input id="input-data-exame" placeholder="Informe a data do exame" type="date"
+        v-model="laudo.data_realizada"></b-input>
+
+        <br>
         
         <label for="input-laudo">Laudo:</label>
-                <b-input id="input-laudo" placeholder=""
-                type="number" min="0"></b-input>
-                <br>
+        <b-form-textarea
+            id="input-text-area"
+            placeholder="Informe o laudo do paciente..."
+            rows="3"
+            max-rows="6"
+            v-model="laudo.laudo"
+        >
+        </b-form-textarea>
+
+        <br>
+
         <label for="input-pdf">PDF:</label>
-                <b-input id="input-pdf" placeholder=""
-                type="number" min="0"></b-input>
-                <br>
-        <b-button pill block variant="danger" @click="registrar_paciente()" class="btn-register">Envier laudo</b-button>
+        <b-form-file
+            placeholder="Insira o PDF aqui..."
+            accept=".pdf"
+            v-model="laudo.imagem"
+        >
+        </b-form-file>
+
+        <br>
+
+        <b-button pill block variant="danger" @click="registrar_laudo()" class="btn-register">Enviar laudo</b-button>
 
       </div>  
   </div>
 </template>
 
 <script>
-export default {
+import { baseApiUrl, showError} from '@/global.js'
+import axios from 'axios'
 
+export default {
+    name: "Registro Laudo",
+    data: () => {
+        return {
+            laudo: {}
+        }
+    },
+    methods: {
+        registrar_laudo() {
+            let formData = new FormData();
+
+            for (const key in this.laudo) {
+                formData.append(key, this.laudo[key])
+            }
+
+            const url = `${baseApiUrl}/laudo`
+            axios.post(url, formData)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+                })
+                .catch(showError)
+        },
+
+        reset() {
+            this.laudo = {}
+        }
+    }
 }
 </script>
 
